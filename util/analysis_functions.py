6d9a3de7f77371657b82b978624be3c29f06dc49
briardoty <briardoty@gmail.com>
2020-03-04 08:10:46 -0800

    Add remaining motif stats

--- util/analysis_functions.py
+++ util/analysis_functions.py
@@ -15,11 +15,18 @@ def compute_motif_stats(network_group, synapses):
     # motif stats...
     p = W.sum() / N**2
 
-    q_div = 0
+    q_div = q_con = q_ch = q_rec = 0
     for i in range(N):
         for j in range(N):
+            q_rec += W[i,j] * W[j,i]
             for k in range(N):
                 q_div += W[i,k] * W[j,k]
+                q_con += W[i,k] * W[i,j]
+                q_ch += W[i,j] * W[j,k]
 
-    q_div = (q_div - p**2) / N**3
-    return (p, q_div)
+
+    q_div = (q_div / N**3) - p**2
+    q_con = (q_con / N**3) - p**2
+    q_ch = (q_ch / N**3) - p**2
+    q_rec = (q_rec / N**3) - p**2
+    return (p, q_div, q_con, q_ch, q_rec)
